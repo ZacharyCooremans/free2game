90f5b4ef7b680562110819413d6e1cfb38fa189c
ZacharyCooremans <77285580+ZacharyCooremans@users.noreply.github.com>
2021-09-06 22:34:50 -0500

    styled the collection page

--- src/components/AllGames.js
+++ src/components/AllGames.js
@@ -6,22 +6,6 @@ import GameCard from './GameCard'
 const AllGames = () => {
     const [games, setGames] = useState([])
 
-    // const options = {
-    //     method: 'GET',
-    //     url: 'https://free-to-play-games-database.p.rapidapi.com/api/games',
-    //     headers: {
-    //         'x-rapidapi-host': 'free-to-play-games-database.p.rapidapi.com',
-    //         'x-rapidapi-key': 'a1999b0534msh272354eea50082cp130fbbjsn9dcc7249f234'
-    //     }
-    // };
-
-    // axios
-    // .request(options).then(function (response) {
-    //     setGames(response.data);
-    // })
-    // .catch(function (error) {
-    //     console.error(error);
-    // });
     useEffect(() => {
         axiosWithAuth()
         .get('/games')
@@ -38,9 +22,9 @@ const AllGames = () => {
             <Container>
                 {games.map((game) => {
                     return(
-                        <div> 
-                            <GameCard key = {game.id} game={game} />
-                        </div>
+                        <Card> 
+                            <GameCard key={game.id} game={game} />
+                        </Card>
                     )
                 })}
             </Container>
@@ -54,7 +38,10 @@ const Container = styled.div`
     display: flex;
     background-color: #6b7994;
     color: #d6d9d7;
-    border: 2px solid red;
     flex-wrap: wrap;
-    align-content: space-between;
+    justify-content: space-between;
+    align-items: center;
+`
+const Card = styled.div`
+    margin: 10px;
 `
