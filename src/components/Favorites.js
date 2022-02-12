64e5511e5be7353ff0766e3b2576f4f63fe96b6e
ZacharyCooremans <77285580+ZacharyCooremans@users.noreply.github.com>
2022-02-11 19:33:42 -0600

    removed the old way I was trying to map also added hover to button

--- src/components/Favorites.js
+++ src/components/Favorites.js
@@ -1,31 +1,45 @@
 import React, { useEffect, useState } from 'react'
-import axios from 'axios'
+import { axiosWithAuth } from '../utils/axiosWithAuth'
 
 const Favorites = () => {
 
-    // NEED TO LEARN HOW TO MAP THROUGH IDS INTO AXIOS
+    const [test, setTest] = useState([])
 
+    // NEED TO LEARN HOW TO MAP THROUGH IDS INTO AXIOS
     const [data, setData] = useState([])
     useEffect(() => {
         setData(JSON.parse(localStorage.getItem('favs')))
     }, [])
     console.log(data)
 
-    const options = {
-        method: 'GET',
-        url: 'https://free-to-play-games-database.p.rapidapi.com/api/game',
-        params: {id: 3},
-        headers: {
-            'x-rapidapi-host': 'free-to-play-games-database.p.rapidapi.com',
-            'x-rapidapi-key': 'a1999b0534msh272354eea50082cp130fbbjsn9dcc7249f234'
-    }
-};
+//     const options = {
+//         method: 'GET',
+//         url: 'https://free-to-play-games-database.p.rapidapi.com/api/game',
+//         params: {id: 3},
+//         headers: {
+//             'x-rapidapi-host': 'free-to-play-games-database.p.rapidapi.com',
+//             'x-rapidapi-key': 'a1999b0534msh272354eea50082cp130fbbjsn9dcc7249f234'
+//     }
+// };
 
-    axios.request(options).then(function (response) {
-        console.log(response.data);
-    }).catch(function (error) {
-        console.error(error);
-    });
+//     axios.request(options).then(function (response) {
+//         console.log(response.data);
+//     }).catch(function (error) {
+//         console.error(error);
+//     });
+
+useEffect(() => {
+    axiosWithAuth()
+    .get('/games')
+    .then((res) => {
+        setTest(res.data)
+    })
+    .catch((err) => {
+        console.log(err)
+    })
+}, [])
+
+console.log(test[0])
 
     return(
         <div>
