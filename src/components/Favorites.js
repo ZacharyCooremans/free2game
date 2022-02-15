22d36c6e455d2537c2e302e54f7e7fc5ef278d14
Zachary Cooremans <zacharycooremans@Zacharys-MacBook-Pro.local>
2022-02-15 16:15:45 -0600

    Fixed the double push and displaying correctly

--- src/components/Favorites.js
+++ src/components/Favorites.js
@@ -1,17 +1,18 @@
 import React, { useEffect, useState } from 'react'
 import { axiosWithAuth } from '../utils/axiosWithAuth'
+import styled from 'styled-components'
 import GameCard from './GameCard'
 
 const Favorites = () => {
 
     const [test, setTest] = useState([])
-    const [data, setData] = useState([])
     const [favId, setFavId] = useState([])
-
-    // NEED TO LEARN HOW TO MAP THROUGH IDS INTO AXIOS
+    const data = []
     
     useEffect(() => {
+        // Grabs my fav id from local
         setFavId(JSON.parse(localStorage.getItem('favs')))
+        //Axios call to get all games again
         axiosWithAuth()
         .get('/games')
         .then((res) => {
@@ -22,25 +23,38 @@ const Favorites = () => {
         })
     }, [])
 
+    // Displays games thats id matches my favs id
     for(let i = 0; i < test.length; i++) {
         if(favId.includes(test[i].id)) {
             data.push(test[i])
         }
     }
-console.log(data)
 
     return(
         <div>
-            <h1>Test FAV</h1>
-            {data.map((game) => {
-                    return(
-                        <div key={game.id} > 
-                            <GameCard game={game} />
-                        </div>
-                    )
-                })}
+            <h1>Favorite Games</h1>
+            <Container>
+                {data.map((game) => {
+                        return(
+                            <Card key={game.id} > 
+                                <GameCard game={game} />
+                            </Card>
+                        )
+                    })}
+            </Container>
         </div>
     )
 }
+
+const Container = styled.div`
+    display: flex;
+    background-color: #6b7994;
+    color: #d6d9d7;
+    flex-wrap: wrap;
+    justify-content: space-around;
+`
+const Card = styled.div`
+    margin: 15px;
+`
 
 export default Favorites
